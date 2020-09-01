6a6e8ad8141c7ada78275db5a98537126e402fca
Inee Ader <xoliio@gmail.com>
2020-09-01 20:04:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,4 @@
-def loop_message_five_times(message)
+def loop_message_five_times (message)
   counter = 0
   while counter < 5 do
     puts message
@@ -10,6 +10,14 @@ def loop_message_n_times (message, n)
   counter = 0  
   while counter <= n do 
     puts message
+    counter += 1 
+  end
+end
+
+def output_array (array) 
+  counter = 0 
+  while counter < array.length do 
+    puts array[counter]
     counter += 1 
   end
 end
