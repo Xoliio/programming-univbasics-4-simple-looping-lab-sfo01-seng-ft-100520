ce0cd10eba5798d294652422f4c6e23ccf0c148a
Inee Ader <xoliio@gmail.com>
2020-09-01 20:08:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,6 +26,8 @@ def return_string_array (array)
   new_array = []
   counter = 0 
   while counter < array.length do
-  new_array.push (array.length.to_s)
+    new_array.push (array[counter])
+    counter += 1
+  end
   return new_array
 end
